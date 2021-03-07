dc5778dcf57cddb09931ba05bfc1eb4396f1b742
Elliot Nash <elliotnash@gmail.com>
2021-03-06 17:41:07 -0800

    fix install / uninstall

--- install.sh
+++ install.sh
@@ -5,7 +5,16 @@ function main() {
     local DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
     cargo install --force --path $DIR
 
-    # make changes to .profile for tab complete
+    # make changes to .zshrc for tab complete
+    local LINE="complete -C __musht_completions musht"
+
+    # bash
+    local FILE="$HOME/.bashrc"
+    grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
+    # zsh
+    local FILE="$HOME/.zshrc"
+    grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 
 }
 
--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,19 @@
 
 function main() {
     local DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-    cargo uninstall musht   
+    cargo uninstall musht
+
+    # remove tab complete rcs
+    local LINE="complete -C __musht_completions musht"
+
+    # bash
+    local FILE="$HOME/.bashrc"
+    echo "$(grep -v "$LINE" $FILE)" >$FILE
+
+    # zsh
+    local FILE="$HOME/.zshrc"
+    echo "$(grep -v "$LINE" $FILE)" >$FILE
+
 }
 
-main()
+main
