04f5a698a76e606e4349498383a356f4aca99ceb
Elliot Nash <elliotnash@gmail.com>
2021-03-06 20:52:05 -0800

    use txt records for multiple ports

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ a mosh client wrapper written in rust with support for srv records
 
 make sure you've added $HOME/.cargo/bin to your path
 
+to use the srv record like feature, you need a txt record and an a/aaaa/cname record under the same name
+
 ./install
 ./uninstall
--- src/args/host.rs
+++ src/args/host.rs
@@ -1,6 +1,13 @@
 use super::MushtArgs;
 use trust_dns_resolver::Resolver;
 use trust_dns_resolver::config::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TxtData {
+    ssh_port: String,
+    mosh_ports: String
+}
 
 impl MushtArgs {
     pub fn resolve_ports(&mut self) -> &mut Self{
@@ -8,22 +15,20 @@ impl MushtArgs {
         // Construct a new Resolver with default configuration options
         let resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
 
-        // Lookup the IP addresses associated with a name.
-        match resolver.srv_lookup(format!("_ssh._tcp.{}.", self.host)){
+        // get txt record
+        match resolver.txt_lookup("i5.server.elliotnash.org."){
             Ok(srv) => {
-                let response = srv.iter().next().unwrap();
-
-                //remove trailing . if present
-                let mut target = response.target().to_string();
-                if target.ends_with(".") {
-                    target.pop();
-                }
-                self.host = target;
-
+                let data = srv.iter().next().unwrap().to_string();
+                let json_result = serde_json::from_str(&data);
+                let json: TxtData = if json_result.is_ok() {
+                    json_result.unwrap()
+                } else {
+                    return self;
+                };
+    
                 //only want to change ports if theyre empty to allow manually specifying ports
-                let port = response.port().to_string();
-                if self.mosh_port == "" {self.mosh_port = port.clone();}
-                if self.ssh_port == "" {self.ssh_port = port;}
+                if self.mosh_port == "" {self.mosh_port = json.mosh_ports;}
+                if self.ssh_port == "" {self.ssh_port = json.ssh_port;}
                 
             },
             Err(_) => {}
